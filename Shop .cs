7fd50ca26a6f815a9ae82047c2feb140e4f9fcda
SasiKudasi <sasha.mittal@yandex.ru>
2024-03-26 16:22:23 +0300

    Добавьте файлы проекта.

---
+++ Shop .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularCustomer
+{
+    internal class Shop
+    {
+        void Add(string name)
+        {
+
+        }
+        void Remove(int id)
+        {
+
+        }
+    }
+}

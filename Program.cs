9e5c342a951d3de6afc57f82cdd0dc90905d82a0
SasiKudasi <sasha.mittal@yandex.ru>
2024-04-02 17:43:34 +0300

    Добавлена возможность переключаться в меню, через нажатия клавиш, в замен ввода символов

--- Program.cs
+++ Program.cs
@@ -5,37 +5,40 @@
         static void Main(string[] args)
         {
             var customer = new Customer();
-            var shop = new Shop();
-            var cmd = "";
+            var shop = new Shop();           
             customer.OnItemChanged(shop);
-            
+
             while (true)
             {
+                
                 Console.WriteLine("Для добавления нажмите клавишу A\nДля удаления нажмите клавишу D");
-                cmd = Console.ReadLine();
+                var key = Console.ReadKey();
                 int i = 0;
                 int itemID = 0;
                 string name = "";
+                var cmd = key.KeyChar;
                 switch (cmd)
                 {
-                    case "A":
+                    case 'A':
                         Console.WriteLine("Введите название товара: ");
                         name = Console.ReadLine();
-                        name +=$" от {DateTime.Now}";
+                        name += $" от {DateTime.Now}";
                         shop.Add(name, i);
                         i++;
                         break;
-                    case "D":
+                    case 'D':
                         Console.WriteLine("Укажите идентификатор товара, который хотите удалить");
                         itemID = int.Parse(Console.ReadLine());
                         shop.Remove(itemID);
                         break;
+                    case 'X':
+                        Environment.Exit(0);
+                        break;
                     default:
                         Console.WriteLine("Нет такой команды");
-                    break;
+                        break;
                 }
-            }          
-            shop.Remove(1);
+            }
         }
     }
 }
